1d16a0f2452350fab86ae37ad55b3e77c401cffc
robo1412 <146008562+robo1412@users.noreply.github.com>
2023-11-29 03:58:16 +0400

    first commit

---
+++ scripts/variables.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Enter your name")
+
+let lastName = prompt("Enter your last name")
+
+let age = prompt("Enter your age")
+
+let address = prompt("Enter your adress")
+
+alert("გამარჯობა "+ firstName + " " + lastName +", მიხარია რომ უკვე "+ age + " წლის ხარ და "+ address + "-ზე ცხოვრობ");
